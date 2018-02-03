2e8daef57638609a1b0b44cae26c2a6925a1f5ed
pvgrajalesg <pvgrajalesg@unal.edu.co>
2018-02-02 22:07:30 -0500

    starting module main-nav

--- src/modules/main-nav/MainNav.js
+++ src/modules/main-nav/MainNav.js
@@ -1,9 +1,11 @@
 import template from './MainNav.pug'
 import './MainNav.scss'
-//import { mainNav } from './../../data/data.json'
+import { mainNav } from './../../../data/home.json'
 
 export default class MainNav {
   constructor (node) {
+    console.log(mainNav.logo.img)
+    this.node = node
     this.node.innerHTML = template(mainNav)
   }
 }
