88ac4dcf2520d4db12fc99583421bd0b0742effd
pvgrajalesg <pvgrajalesg@unal.edu.co>
2018-02-07 22:07:02 -0500

    adding x in menu

--- src/modules/main-nav/MainNav.js
+++ src/modules/main-nav/MainNav.js
@@ -4,20 +4,24 @@ import { mainNav } from './../../data/home.json'
 
 export default class MainNav {
   constructor (node) {
-    console.log(mainNav.logo.img)
     this.node = node
     this.node.innerHTML = template(mainNav)
     this.eventButtonMenu()
   }
 
   eventButtonMenu () {
-    console.log(this.node)
-    const buttonMenu = this.node.querySelector('.button')
-    console.log(buttonMenu)
+    const buttonMenu = this.node.querySelector('.button-closed')
     const mainNav = this.node.querySelector('.main-nav-menu')
+    const buttonCross = this.node.querySelector('.button-opened')
     console.log(mainNav)
     buttonMenu.addEventListener('click', function () {
       mainNav.classList.toggle('main-nav-menu--opened')
+      buttonCross.classList.toggle('button-opened--js')
+    })
+
+    buttonCross.addEventListener('click', function () {
+      mainNav.classList.toggle('main-nav-menu--opened')
+      buttonCross.classList.toggle('button-opened--js')
     })
   }
 }
