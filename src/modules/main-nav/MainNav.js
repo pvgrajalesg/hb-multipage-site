1fd6235ae8b49d1aad111c2c5c5870fbd80249f4
pvgrajalesg <pvgrajalesg@unal.edu.co>
2018-02-14 09:22:44 -0500

    changing name class css

--- src/modules/main-nav/MainNav.js
+++ src/modules/main-nav/MainNav.js
@@ -11,26 +11,25 @@ export default class MainNav {
   }
 
   eventButtonMenu () {
-    const buttonMenu = this.node.querySelector('.button-closed')
-    const mainNav = this.node.querySelector('.main-nav-menu')
-    const buttonCross = this.node.querySelector('.button-closed-line')
-    console.log(mainNav)
+    const buttonMenu = this.node.querySelector('.main-nav-container__button-closed')
+    const mainNav = this.node.querySelector('.main-nav-menu__list')
+    const buttonCross = this.node.querySelector('.main-nav-container__button-closed--line')
     buttonMenu.addEventListener('click', function () {
-      mainNav.classList.toggle('main-nav-menu--opened')
-      buttonCross.classList.toggle('button-closed-line')
-      buttonCross.classList.toggle('button-closed-cross')
+      mainNav.classList.toggle('main-nav-menu__list--opened')
+      buttonCross.classList.toggle('main-nav-container__button-closed--line')
+      buttonCross.classList.toggle('main-nav-container__button-closed--line')
     })
   }
 
   eventOptionMenu () {
-    const mainNav = this.node.querySelector('.main-nav-menu')
+    const mainNav = this.node.querySelector('.main-nav-menu__list')
     mainNav.addEventListener('click', this.show.bind(this))
   }
 
   show(event){
-    if (event.target.parentElement.className === 'item-dropdown__button') {
-      const dropdownMenu = event.target.parentElement.parentElement.parentElement.querySelector('.item-dropdown__menu')
-      dropdownMenu.classList.toggle('item-dropdown--opened')
+    if (event.target.parentElement.className === 'main-nav-menu__list__item-dropdown__button') {
+      const dropdownMenu = event.target.closest('.main-nav-menu__list__item').querySelector('.main-nav-menu__list__item-menu ')
+      dropdownMenu.classList.toggle('main-nav-menu__list__item-dropdown--opened')
     }
   }
 }
