c9e6113797da84bd9f6efd7a2bcb886351f8d26b
pvgrajalesg <pvgrajalesg@unal.edu.co>
2018-02-19 10:21:43 -0500

    Correcting styles in module hero-content

--- src/modules/hero-content/HeroContent.js
+++ src/modules/hero-content/HeroContent.js
@@ -2,7 +2,7 @@ import template from './HeroContent.pug'
 import './HeroContent.scss'
 import heroContent from './../../data/heroContent.json'
 
-export default class MainNav {
+export default class HeroContent {
   constructor (node) {
     this.node = node
     this.node.innerHTML = template(heroContent)
