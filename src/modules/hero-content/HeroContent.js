2e2f7354c8f54c23c8aebeda0c8a9be7f6d031c8
pvgrajalesg <pvgrajalesg@unal.edu.co>
2018-02-21 15:18:17 -0500

    adding changes pull request

--- src/modules/hero-content/HeroContent.js
+++ src/modules/hero-content/HeroContent.js
@@ -1,10 +1,16 @@
 import template from './HeroContent.pug'
 import './HeroContent.scss'
-import heroContent from './../../data/heroContent.json'
+import heroContent from './../../data/home.json'
 
 export default class HeroContent {
   constructor (node) {
     this.node = node
+    this.dataHero()
+    console.log(heroContent)
     this.node.innerHTML = template(heroContent)
+  }
+
+  dataHero () {
+    heroContent.push({'size': '639px'})
   }
 }
